d71f229fef83fc9ba448b55979a3ccc078972de1
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-09 06:51:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this whole example doesn't work *shrug*
+            checked
+            {
+                byte number = 255;
+                number = number + 1;
+            }
+            
+            //byte number = 255;
+            //number = number + 1; // instead of 0 a program doesn't compile
+            //Console.WriteLine(number);
+        }
+    }
+}
